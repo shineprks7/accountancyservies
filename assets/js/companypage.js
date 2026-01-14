c3dba33a02c75001a8a8e060eb745da77909c801
shineprks7 <prakash.p@hoostack.com>
2026-01-14 09:41:07 +0530

    changes

--- assets/js/companypage.js
+++ assets/js/companypage.js
@@ -10,6 +10,66 @@
 
         const companyservicesbtns = document.querySelectorAll('.company-services-list-item-wrapper');
 
+                const servicebacktopagebtns = document.querySelectorAll('.services-back-to-page-btn');
+
+                const servicecards = document.querySelectorAll('.company-services-list-item-card');
+
+
+
+        const newinnerHTML = "<a class='services-back-to-page-btn-wrapper '> <button class = 'services-back-to-page-btn '> <img src='assets/images/icons/direction-icon-blue.svg' />  <span class=''> Back to Services</span> </button> </a> ";
+
+
+           servicecards.forEach((servicecard) => 
+
+    {
+
+        
+    
+          servicecard.insertAdjacentHTML('beforeend', newinnerHTML);
+
+           const servicebtn = servicecard.querySelector('.services-back-to-page-btn');
+
+
+                servicebtn.addEventListener('click', function( event ){
+
+
+           const thisbutton = event.currentTarget;
+
+
+
+            const currentparent = thisbutton.closest('.company-page-core-services-list-grid-column');
+
+           const superparent = thisbutton.closest('.company-page-core-services-list-grid');
+
+
+           const siblings = superparent.querySelectorAll('.company-page-core-services-list-grid-column');
+
+           const checkclass = currentparent.classList.contains('active');
+
+
+
+
+           
+
+           
+
+            currentparent.classList.remove('active');
+
+            document.body.style.overflow = "scroll";
+
+          
+
+
+
+
+
+
+
+});
+
+    });
+
+
 
          companyservicesbtns.forEach((servicebtn => 
 
@@ -77,6 +137,51 @@
              
  
            }
+
+
+
+
+
+
+
+});
+
+
+    }
+))
+
+      servicebacktopagebtns.forEach((servicebtn => 
+
+    {
+
+        servicebtn.addEventListener('click', function( event ){
+
+
+           const thisbutton = event.currentTarget;
+
+
+
+            const currentparent = thisbutton.closest('.company-page-core-services-list-grid-column');
+
+           const superparent = thisbutton.closest('.company-page-core-services-list-grid');
+
+
+           const siblings = superparent.querySelectorAll('.company-page-core-services-list-grid-column');
+
+           const checkclass = currentparent.classList.contains('active');
+
+
+
+
+           
+
+           
+
+            currentparent.classList.remove('active');
+
+            document.body.style.overflow = "scroll";
+
+          
 
 
 
